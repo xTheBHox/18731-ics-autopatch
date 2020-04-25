82665e57c304f3b4eeab83f6fe6383d1f99f683c
Snort Server <snortserver>
2020-04-25 01:43:51 +0000

    debugging

--- modbus_server.py
+++ modbus_server.py
@@ -1,14 +1,14 @@
 import socket
-import threading 
+import random
+import threading
 
-CLIENT_IP = '10.1.1.1'
-SERVER_IP = '10.1.2.1'
+CLIENT_IP = '192.168.1.100'
+SERVER_IP = '192.168.1.104'
 SERVER_PORT = 502
 
-class MBReply():
+class MB():
     
     UNIT_ID = 1
-    
     FNCODE_READ_DISCRETE_INPUTS = 2
     FNCODE_READ_COILS = 1
     FNCODE_WRITE_SINGLE_COIL = 5
@@ -17,44 +17,57 @@ class MBReply():
     FNCODE_READ_MULTIPLE_HOLDING_REGISTERS = 3
     FNCODE_WRITE_SINGLE_HOLDING_REGISTER = 6
     FNCODE_WRITE_MULTIPLE_HOLDING_REGISTERS = 6
+
+class MBReply():
     
     def __init__(self, b):
         l = int.from_bytes(b[4:6], byteorder='big', signed=False) + 6
         if l != len(b):
             print("Warning: length mismatch")
-        self,tn = b[0:2]
+        self.tn = b[0:2]
         self.fn = b[7]
-        if fn == FNCODE_READ_COILS:
-            bits = int.from_bytes(b[8:10], byteorder='big', signed=False) + 6
+        if self.fn == MB.FNCODE_READ_COILS or self.fn == MB.FNCODE_READ_DISCRETE_INPUTS:
+            bits = int.from_bytes(b[8:10], byteorder='big', signed=False)
             size = ((bits - 1) // 8) + 1
             self.data = bytearray(size + 1)
             self.data[0] = size
-            self.len = size + 3
-        elif fn == FNCODE_READ_DISCRETE_INPUTS:
-            pass
+            
+            # TODO: maybe randomize the actual reply values
+            
+        elif self.fn == MB.FNCODE_READ_MULTIPLE_HOLDING_REGISTERS:
+            reg_count = int.from_bytes(b[10:12], byteorder='big', signed=False)
+            self.data = bytearray(2 * reg_count + 1)
+            self.data[0] = 2 * reg_count
+            for i in range(reg_count):
+                self.data[i*2+1:i*2+3] = random.randint(1, 0xffff).to_bytes(2, byteorder='big', signed=False)
         
     def send(self, sock):
         b = bytearray(8)
-        b[0:2] = self.tn.to_bytes(2, byteorder='big', signed=False)
-        b[4:6] = (2 + self.len).to_bytes(2, byteorder='big', signed=False)
-        b[6] = MBPkt.UNIT_ID.to_bytes
+        b[0:2] = self.tn
+        b[4:6] = (len(self.data) + 2).to_bytes(2, byteorder='big', signed=False)
+        b[6] = MB.UNIT_ID
         b[7] = self.fn
+        
         b = b + self.data
-        i = 0 
+        print("Reply length: %d" % len(b))
+        i = 0
         while i < len(b):
             i += sock.send(b[i:], len(b) - i)
+            print("Sent %d" % i)
     
 def thread_fn(sock):
     buf = bytearray()
     while True:
-        msg = sock.recv()
+        msg = sock.recv(256)
         if len(msg) == 0: break
         buf.extend(msg)
         if len(buf) < 6: continue
         l = int.from_bytes(buf[4:6], byteorder='big', signed=False) + 6
+        print("%d bytes needed," % l, "%d bytes buffered" % len(buf))
         if len(buf) < l: continue
-        P = MBPkt(buf[:l])
-        P.send()
+        print("Replying")
+        P = MBReply(buf[:l])
+        P.send(sock)
         buf = buf[l:]
         
 threads = []
@@ -63,11 +76,20 @@ s.bind(('', SERVER_PORT))
 s.setblocking(False)
 s.listen()
 while True:
-    sock, addr = s.accept()
+    try:
+        try:
+            sock, addr = s.accept()
+        except BlockingIOError:
+            continue
+    except KeyboardInterrupt:
+        break
     sock.setblocking(True)
-    th = threading.Thread(target=thread_fn, args=sock)
+    th = threading.Thread(target=thread_fn, args=(sock,))
     threads.append(th)
     th.start()
     for th in threads:
         th.join(0)
-
+        threads = list(filter(lambda t: t.is_alive(), threads))
+        
+print("Closing server.")
+s.close()
