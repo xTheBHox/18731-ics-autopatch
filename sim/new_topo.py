3ced7e4e0f82f57816588c78f748eabfcca609c7
Snort Server <snortserver>
2020-05-05 02:06:24 +0000

    pcap files

--- sim/new_topo.py
+++ sim/new_topo.py
@@ -13,25 +13,37 @@ class create_topo(Topo):
 
         h1 = self.addHost( 'h1' , ip = '192.168.1.100')
         h2 = self.addHost( 'h2' , ip = '192.168.1.101')
+        #h3 = self.addHost( 'h3' , ip = '192.168.1.101')
+        h3 = self.addHost( 'h3', ip = '192.168.1.110')
+        h4 = self.addHost( 'h4', ip = '192.168.1.111')
+
         s1 = self.addSwitch( 's1' )
         s2 = self.addSwitch( 's2' )
 
+
+
         self.addLink( h1,s1 )
         self.addLink( h2,s2 )
-        self.addLink( s1,s2 )
+        self.addLink( h3,s1 )
+        self.addLink( h4,s2 )
+        #self.addLink( s1,s2 )
+        #self.addLink( s1,h3 )
+    
 
 def start_cli():
     topo = create_topo()
     net = Mininet(topo=topo)
     net.start()
     dumpNodeConnections(net.hosts)
-    s1, s2, h1, h2 = net.get('s1', 's2', 'h1', 'h2')
+    s1, s2, h1, h2, h3, h4 = net.get('s1', 's2', 'h1', 'h2', 'h3', 'h4')
     net.pingAll()
+    #net.configLinkStatus('s1', 's2', 'down')
+    #net.pingAll()
     h1.cmd('ethtool --offload h1-eth0 rx off tx off')
     h2.cmd('ethtool --offload h2-eth0 rx off tx off')
     h2.cmd('python3 modbus_server.py &')
-    h1.cmd('tcpdump -i h1-eth0 -w h1_without_snort.pcap &')
-    h2.cmd('tcpdump -i h2-eth0 -w h2_without_snort.pcap &')
+    h1.cmd('tcpdump -i h1-eth0 -w h1.pcap &')
+    h2.cmd('tcpdump -i h2-eth0 -w h2.pcap &')
     CLI(net)
     net.stop()
 
